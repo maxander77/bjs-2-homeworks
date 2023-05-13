e0dce7f4ccb281dda1c947c3455ca634233cf245
Maxander <maxander77@gmail.com>
2023-05-13 17:10:41 +0200

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  const discriminant = b ** 2 - 4 * a * c;
+
+    if (discriminant < 0) {
+        return arr;
+    } else if (discriminant === 0) {
+        const root = -b / (2 * a);
+        arr.push(root);
+    } else {
+        const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        arr.push(root1, root2);
+    }
   
   return arr;
 }
